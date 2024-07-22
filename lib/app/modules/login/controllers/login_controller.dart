d257f984366d5dce1e46b4220ab53c9e95f26b76
dapraws <darrelprawira26@gmail.com>
2024-07-23 02:03:14 +0700

    feat: login view

--- lib/app/modules/login/controllers/login_controller.dart
+++ lib/app/modules/login/controllers/login_controller.dart
@@ -1,23 +1,117 @@
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+// import 'package:getcrew_crew_v2/data/local/get_utils.dart' as get_utils;
+// import 'package:getcrew_crew_v2/services/authentication_service.dart';
+// import 'package:getcrew_crew_v2/ui/forget_password/forget_password_page.dart';
+// import 'package:getcrew_crew_v2/ui/register/register_page.dart';
+// import 'package:getcrew_crew_v2/ui/splash_success/splash_success_page.dart';
+// import 'package:getcrew_crew_v2/ui/widgets/show_custom_dialog.dart';
+// import 'package:getcrew_crew_v2/utils/logging.dart';
+// import 'package:getcrew_crew_v2/utils/validation_helpers.dart';
 
 class LoginController extends GetxController {
-  //TODO: Implement LoginController
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
 
-  final count = 0.obs;
-  @override
-  void onInit() {
-    super.onInit();
+  var isPasswordHide = true.obs;
+  var isLoadingLogin = false.obs;
+
+  var formValid = false.obs;
+  var correctEmail = false.obs;
+  var correctPassword = false.obs;
+
+  // LoginPageController() {
+  //   _addListeners();
+  // }
+
+  // void _addListeners() {
+  //   _addControllerListener(
+  //     emailController,
+  //     ValidationHelpers.validateEmail,
+  //     correctEmail,
+  //   );
+  //   _addControllerListener(
+  //     passwordController,
+  //     (text) => ValidationHelpers.validatePassword(
+  //       value: text,
+  //       isRegister: false,
+  //     ),
+  //     correctPassword,
+  //   );
+  // }
+
+  void _addControllerListener(
+    TextEditingController controller,
+    String? Function(String) validator,
+    RxBool correct,
+  ) {
+    controller.addListener(
+      () {
+        String? textValidate = validator(controller.text);
+        correct.value = textValidate == null;
+        formValid.value = _isFormValid();
+        // log.i('formValid ${formValid.value}');
+      },
+    );
   }
 
-  @override
-  void onReady() {
-    super.onReady();
+  bool _isFormValid() {
+    return correctEmail.value && correctPassword.value;
   }
 
-  @override
-  void onClose() {
-    super.onClose();
+  // void onPressedIconPassword() {
+  //   isPasswordHide.value = !isPasswordHide.value;
+  //   log.i('isPasswordHide ${isPasswordHide.value}');
+  // }
+
+  // String? validatorEmail(String? value) {
+  //   return ValidationHelpers.validateEmail(value ?? "");
+  // }
+
+  // String? validatorPassword(String? value) {
+  //   return ValidationHelpers.validatePassword(
+  //     value: value ?? "",
+  //     isRegister: false,
+  //   );
+  // }
+
+  VoidCallback? onLoginClicked() {
+    return formValid.value ? login : null;
   }
 
-  void increment() => count.value++;
+  // void onForgetPasswordClicked() {
+  //   Get.to(() => const ForgetPasswordPage());
+  // }
+
+  // void onRegisterClicked() {
+  //   Get.to(() => const RegisterPageView());
+  // }
+
+  void login() async {
+    isLoadingLogin.value = true;
+    // final authService = AuthenticationService();
+    // final loginSuccess = await authService.login(
+    //     email: emailController.text, password: passwordController.text);
+    // get_utils.GetUtils prefs = get_utils.GetUtils();
+    // if (!loginSuccess) {
+    //   final errorMessage = authService.message ?? 'Unknown error';
+    //   showCustomDialog(
+    //     title: 'Data Tidak Sesuai',
+    //     message: errorMessage,
+    //   );
+    //   isLoadingLogin.value = false;
+    // } else {
+    // Get.offAll(
+    //   () => const SplashSuccessPage(),
+    //   arguments: {
+    //     'message': 'Kamu sudah berhasil masuk',
+    //     'routeAfterSplash': 'bottomNavBar',
+    //   },
+    // );
+
+    emailController.clear();
+    passwordController.clear();
+    // prefs.setIsGuest(false);
+    isLoadingLogin.value = false;
+  }
 }
--- lib/app/modules/login/views/login_view.dart
+++ lib/app/modules/login/views/login_view.dart
@@ -1,22 +1,96 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 import 'package:get/get.dart';
+import 'package:homevice/app/common/theme/fonts.dart';
+import 'package:homevice/app/common/theme/theme.dart';
+import 'package:homevice/app/modules/main/views/main_view.dart';
+import 'package:homevice/app/shared/widgets/custom_elevated_button.dart';
+import 'package:homevice/app/shared/widgets/custom_text_field.dart';
 
 import '../controllers/login_controller.dart';
 
 class LoginView extends GetView<LoginController> {
-  const LoginView({Key? key}) : super(key: key);
+  const LoginView({super.key});
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('LoginView'),
-        centerTitle: true,
-      ),
-      body: const Center(
-        child: Text(
-          'LoginView is working',
-          style: TextStyle(fontSize: 20),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            SvgPicture.asset(
+              'assets/svg/Logo.svg',
+              height: MediaQuery.sizeOf(context).height * 0.13,
+            ),
+            const SizedBox(height: 75),
+            CustomTextField(
+              label: 'Email or phone number',
+              hintText: '',
+              labelStyle: regularText16,
+              isAutoValidate: false,
+              isRequired: false,
+              isPassword: false,
+              isPasswordEmpty: false,
+              isPasswordHide: false,
+              textInputType: TextInputType.emailAddress,
+              controller: controller.emailController,
+            ),
+            const SizedBox(height: 20),
+            CustomTextField(
+              label: 'Password',
+              hintText: '',
+              labelStyle: regularText16,
+              isAutoValidate: false,
+              isRequired: false,
+              isPassword: true,
+              isPasswordEmpty: false,
+              isPasswordHide: false,
+              textInputType: TextInputType.visiblePassword,
+              controller: controller.passwordController,
+            ),
+            Align(
+              alignment: Alignment.centerRight,
+              child: TextButton(
+                child: Text(
+                  "Forget Password?",
+                  style: regularText12,
+                ),
+                onPressed: () {},
+              ),
+            ),
+            const SizedBox(height: 20),
+            CustomElevatedButton(
+              onPressed: () {
+                Get.toNamed('/main'); // TODO: switch to Get.offAndToNamed
+              },
+              text: 'Login',
+            ),
+            const SizedBox(height: 50),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'Don’t have an account yet?',
+                  style: regularText12,
+                ),
+                const SizedBox(
+                  width: 2,
+                ),
+                GestureDetector(
+                  onTap: () {},
+                  child: Text(
+                    'Sign up here',
+                    style: regularText12.copyWith(
+                      color: themeData.primaryColor,
+                    ),
+                  ),
+                )
+              ],
+            )
+          ],
         ),
       ),
     );
