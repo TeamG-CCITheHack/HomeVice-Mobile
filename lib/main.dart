6f73fe8fe64f682899525de5def0c2974d00a5be
dapraws <darrelprawira26@gmail.com>
2024-07-31 14:06:21 +0700

    feat: isLogin preferences

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,18 @@
 import 'package:flutter/material.dart';
-
 import 'package:get/get.dart';
-
+import 'package:homevice/app/service/shared_preferences.dart';
 import 'app/routes/app_pages.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final SharedPreferencesService sharedPreferencesService =
+      SharedPreferencesService();
+  final token = await sharedPreferencesService.getToken();
+
   runApp(
     GetMaterialApp(
       title: "Application",
-      initialRoute: AppPages.INITIAL,
+      initialRoute: token != null ? Routes.MAIN : Routes.LOGIN,
       getPages: AppPages.routes,
     ),
   );
