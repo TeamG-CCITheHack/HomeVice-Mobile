a1b8d160d2c7f66a50a3400bd243a1d49c76639f
dapraws <darrelprawira26@gmail.com>
2024-08-05 00:07:24 +0700

    feat: add service page content

--- lib/app/modules/service/views/service_view.dart
+++ lib/app/modules/service/views/service_view.dart
@@ -1,22 +1,57 @@
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
+import 'package:homevice/app/data/data_provider.dart';
+import 'package:homevice/app/shared/widgets/custom_search_bar.dart';
+import 'package:homevice/app/shared/widgets/service_provider_widget.dart';
 
 import '../controllers/service_controller.dart';
 
 class ServiceView extends GetView<ServiceController> {
-  const ServiceView({Key? key}) : super(key: key);
+  final services = serviceDataList();
+
+  ServiceView({super.key});
   @override
   Widget build(BuildContext context) {
+    Get.lazyPut(() => ServiceController());
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('ServiceView'),
-        centerTitle: true,
-      ),
-      body: const Center(
-        child: Text(
-          'ServiceView is working',
-          style: TextStyle(fontSize: 20),
+      backgroundColor: Colors.transparent,
+      extendBodyBehindAppBar: true,
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topLeft,
+            end: Alignment.centerRight,
+            stops: [0.1, 0.2, 0.7],
+            colors: [
+              Color(0xFF499FD4),
+              Color(0xFF3393CF),
+              Color(0xFFFFFFFF),
+            ],
+          ),
+        ),
+        child: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 10.0),
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: CustomSearchBar(
+                      searchController: controller.searchController,
+                    ),
+                  ),
+                  Column(
+                    children: services
+                        .map((service) =>
+                            ServiceProviderWidget(service: service))
+                        .toList(),
+                  )
+                ],
+              ),
+            ),
+          ),
         ),
       ),
     );
