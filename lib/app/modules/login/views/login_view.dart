2968902c995f4a1c9f5a57be0da1e0225b2ce9f4
dapraws <darrelprawira26@gmail.com>
2024-08-04 15:04:14 +0700

    feat: loading indicator

--- lib/app/modules/login/views/login_view.dart
+++ lib/app/modules/login/views/login_view.dart
@@ -13,90 +13,100 @@ class LoginView extends GetView<LoginController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 20.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            SvgPicture.asset(
-              'assets/svg/Logo.svg',
-              height: MediaQuery.of(context).size.height * 0.13,
+      body: Obx(() {
+        if (controller.isLoadingLogin.value) {
+          return const Center(
+            child: CircularProgressIndicator(
+              color: Colors.blue,
             ),
-            const SizedBox(height: 75),
-            CustomTextField(
-              label: 'Email or phone number',
-              hintText: '',
-              labelStyle: regularText16,
-              isAutoValidate: false,
-              isRequired: true,
-              isPassword: false,
-              isPasswordEmpty: false,
-              isPasswordHide: false,
-              textInputType: TextInputType.emailAddress,
-              controller: controller.emailController,
-            ),
-            const SizedBox(height: 20),
-            Obx(
-              () => CustomTextField(
-                label: 'Password',
-                hintText: '',
-                labelStyle: regularText16,
-                isAutoValidate: false,
-                isRequired: true,
-                isPassword: true,
-                isPasswordEmpty: false,
-                isPasswordHide: controller.isPasswordHide.value,
-                textInputType: TextInputType.visiblePassword,
-                controller: controller.passwordController,
-                onPressedIconPassword: () {
-                  controller.isPasswordHide.value =
-                      !controller.isPasswordHide.value;
-                },
-              ),
-            ),
-            Align(
-              alignment: Alignment.centerRight,
-              child: TextButton(
-                child: Text(
-                  "Forget Password?",
-                  style: regularText12,
-                ),
-                onPressed: () {},
-              ),
-            ),
-            const SizedBox(height: 20),
-            Obx(
-              () => CustomElevatedButton(
-                onPressed: controller.onLoginClicked(),
-                text: 'Login',
-              ),
-            ),
-            const SizedBox(height: 50),
-            Row(
+          );
+        } else {
+          return Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20.0),
+            child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
               children: [
-                Text(
-                  'Don’t have an account yet?',
-                  style: regularText12,
+                SvgPicture.asset(
+                  'assets/svg/Logo.svg',
+                  height: MediaQuery.of(context).size.height * 0.13,
                 ),
-                const SizedBox(
-                  width: 2,
+                const SizedBox(height: 75),
+                CustomTextField(
+                  label: 'Email or phone number',
+                  hintText: '',
+                  labelStyle: regularText16,
+                  isAutoValidate: false,
+                  isRequired: true,
+                  isPassword: false,
+                  isPasswordEmpty: false,
+                  isPasswordHide: false,
+                  textInputType: TextInputType.emailAddress,
+                  controller: controller.emailController,
                 ),
-                GestureDetector(
-                  onTap: () {},
-                  child: Text(
-                    'Sign up here',
-                    style: regularText12.copyWith(
-                      color: themeData.primaryColor,
-                    ),
+                const SizedBox(height: 20),
+                Obx(
+                  () => CustomTextField(
+                    label: 'Password',
+                    hintText: '',
+                    labelStyle: regularText16,
+                    isAutoValidate: false,
+                    isRequired: true,
+                    isPassword: true,
+                    isPasswordEmpty: false,
+                    isPasswordHide: controller.isPasswordHide.value,
+                    textInputType: TextInputType.visiblePassword,
+                    controller: controller.passwordController,
+                    onPressedIconPassword: () {
+                      controller.isPasswordHide.value =
+                          !controller.isPasswordHide.value;
+                    },
                   ),
+                ),
+                Align(
+                  alignment: Alignment.centerRight,
+                  child: TextButton(
+                    child: Text(
+                      "Forget Password?",
+                      style: regularText12,
+                    ),
+                    onPressed: () {},
+                  ),
+                ),
+                const SizedBox(height: 20),
+                Obx(
+                  () => CustomElevatedButton(
+                    onPressed: controller.onLoginClicked(),
+                    text: 'Login',
+                  ),
+                ),
+                const SizedBox(height: 50),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Don’t have an account yet?',
+                      style: regularText12,
+                    ),
+                    const SizedBox(
+                      width: 2,
+                    ),
+                    GestureDetector(
+                      onTap: () {},
+                      child: Text(
+                        'Sign up here',
+                        style: regularText12.copyWith(
+                          color: themeData.primaryColor,
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
               ],
             ),
-          ],
-        ),
-      ),
+          );
+        }
+      }),
     );
   }
 }
