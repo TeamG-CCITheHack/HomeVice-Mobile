154142393b15f336129870dde15d5c4a8eaa4df5
dapraws <darrelprawira26@gmail.com>
2024-08-05 00:07:36 +0700

    feat: add splash

--- lib/app/modules/splash/views/splash_view.dart
+++ lib/app/modules/splash/views/splash_view.dart
@@ -1,13 +1,21 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
+import 'package:homevice/app/routes/app_pages.dart';
+import 'package:homevice/app/service/shared_preferences.dart';
 
 import '../controllers/splash_controller.dart';
 
 class SplashView extends GetView<SplashController> {
-  const SplashView({Key? key}) : super(key: key);
+  const SplashView({super.key});
   @override
   Widget build(BuildContext context) {
+    Timer(
+      const Duration(seconds: 3),
+      () => _navigateToNextScreen(), // Navigate after splash
+    );
     return Scaffold(
       appBar: AppBar(
         title: const Text('SplashView'),
@@ -20,5 +28,16 @@ class SplashView extends GetView<SplashController> {
         ),
       ),
     );
+  }
+
+  void _navigateToNextScreen() async {
+    final SharedPreferencesService sharedPreferencesService =
+        SharedPreferencesService();
+    final token = await sharedPreferencesService.getToken();
+    if (token != null) {
+      Get.offAllNamed(Routes.MAIN);
+    } else {
+      Get.offAllNamed(Routes.LOGIN);
+    }
   }
 }
