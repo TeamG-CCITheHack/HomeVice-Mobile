d6cee4028ab9cbdfc584a4153a4e5b1b9ba0d2a5
dapraws <darrelprawira26@gmail.com>
2024-08-07 01:34:55 +0700

    feat: initializing splash

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:homevice/app/service/shared_preferences.dart';
 import 'app/routes/app_pages.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  final SharedPreferencesService sharedPreferencesService =
-      SharedPreferencesService();
-  final token = await sharedPreferencesService.getToken();
-
   runApp(
     GetMaterialApp(
       title: "Application",
-      initialRoute: token != null ? Routes.MAIN : Routes.LOGIN,
+      initialRoute: Routes.SPLASH,
       getPages: AppPages.routes,
     ),
   );
