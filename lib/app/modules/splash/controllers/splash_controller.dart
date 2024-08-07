3e9aeb6b878fc4e179655bb871462c11f16fa7bc
dapraws <darrelprawira26@gmail.com>
2024-08-07 23:38:10 +0700

    feat: splash new route

--- lib/app/modules/splash/controllers/splash_controller.dart
+++ lib/app/modules/splash/controllers/splash_controller.dart
@@ -1,23 +1,33 @@
+import 'dart:async';
+
 import 'package:get/get.dart';
+import 'package:homevice/app/routes/app_pages.dart';
+// import 'package:homevice/app/service/jwt_decoder.dart';
+import 'package:homevice/app/service/shared_preferences.dart';
 
 class SplashController extends GetxController {
-  //TODO: Implement SplashController
+  void navigateToNextScreen() async {
+    final SharedPreferencesService sharedPreferencesService =
+        SharedPreferencesService();
+    final token = await sharedPreferencesService.getToken();
+    if (token != null) {
+      Get.offAllNamed(Routes.P_MAIN);
+    } else {
+      Get.offAllNamed(Routes.LOGIN);
+    }
 
-  final count = 0.obs;
-  @override
-  void onInit() {
-    super.onInit();
+    // -------------[]-------------
+    // if (token != null) {
+    //   final role = JWTDecoder.getRoleFromToken(token);
+    //   if (role == 'customer') {
+    //     Get.offAllNamed(Routes.MAIN);
+    //   } else if (role == 'partner') {
+    //     Get.offAllNamed(Routes.P_MAIN);
+    //   } else {
+    //     Get.offAllNamed(Routes.LOGIN);
+    //   }
+    // } else {
+    //   Get.offAllNamed(Routes.LOGIN);
+    // }
   }
-
-  @override
-  void onReady() {
-    super.onReady();
-  }
-
-  @override
-  void onClose() {
-    super.onClose();
-  }
-
-  void increment() => count.value++;
 }
--- lib/app/modules/splash/views/splash_view.dart
+++ lib/app/modules/splash/views/splash_view.dart
@@ -3,9 +3,6 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
-import 'package:homevice/app/routes/app_pages.dart';
-import 'package:homevice/app/service/shared_preferences.dart';
-
 import '../controllers/splash_controller.dart';
 
 class SplashView extends GetView<SplashController> {
@@ -14,7 +11,7 @@ class SplashView extends GetView<SplashController> {
   Widget build(BuildContext context) {
     Timer(
       const Duration(seconds: 3),
-      () => _navigateToNextScreen(), // Navigate after splash
+      () => controller.navigateToNextScreen(),
     );
     return Scaffold(
       appBar: AppBar(
@@ -28,16 +25,5 @@ class SplashView extends GetView<SplashController> {
         ),
       ),
     );
-  }
-
-  void _navigateToNextScreen() async {
-    final SharedPreferencesService sharedPreferencesService =
-        SharedPreferencesService();
-    final token = await sharedPreferencesService.getToken();
-    if (token != null) {
-      Get.offAllNamed(Routes.MAIN);
-    } else {
-      Get.offAllNamed(Routes.LOGIN);
-    }
   }
 }
